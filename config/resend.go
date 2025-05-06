1101fa51124d1988e173b63dcd4e0649c85b369d
yantology <work@yantology.dev>
2025-05-06 13:29:47 +0700

    fix: enhance logging for Resend API configuration to improve error visibility

--- config/resend.go
+++ config/resend.go
@@ -16,18 +16,23 @@ type ResendApi struct {
 
 func InitResendConfig() (*ResendApi, *customerror.CustomError) {
 	apiKey := os.Getenv("RESEND_API_KEY")
+	log.Println("Resend API key => ", apiKey != "")
 	if apiKey == "" {
 		log.Println("Resend API key is not set")
 		return nil, customerror.NewCustomError(nil, "Resend API key is not set", http.StatusUnauthorized)
 	}
+
 	resendDomain := os.Getenv("RESEND_DOMAIN")
 	log.Println("Resend domain => ", resendDomain)
 	if resendDomain == "" {
+		log.Println("Resend domain is not set")
 		return nil, customerror.NewCustomError(nil, "Resend domain is not set", http.StatusUnauthorized)
 	}
 
 	resendName := os.Getenv("RESEND_NAME")
+	log.Println("Resend name => ", resendName)
 	if resendName == "" {
+		log.Println("Resend name is not set")
 		return nil, customerror.NewCustomError(nil, "Resend name is not set", http.StatusUnauthorized)
 	}
 
